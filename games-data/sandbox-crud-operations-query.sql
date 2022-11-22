6cbcc7bfc2f4fb169ce93e2df3ff79ea22127524
[Kevin] <[kevin.salinas04@utrgv.edu]>
2022-11-22 13:24:58 -0600

    add games data

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int32_value, double_value, int16_value) VALUES (77742069, 572, 2849);
+
+--UPDATE sandbox SET int64_value = 420 WHERE id = 72;
+
+--DELETE FROM sandbox WHERE id = 73;
+
+SELECT * FROM sandbox;
